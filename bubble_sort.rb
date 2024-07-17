d462f7ee8c9a49b233dba35ed1263a9093b837d0
t-drape <thebatreview1@gmail.com>
2024-07-17 13:32:53 -0700

    Add all pseudocode and comments

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+# Step 1:
+  # Get input array of numbers from user
+# Step 2:
+  # Create swap counter variable
+# Step 3: 
+  # For each element in array,
+# Step 3a:
+  # Compare element to next element,
+# Step 3aa:
+  # If next element is smaller, swap
+# Step 3ab:
+  # Else, keep same order
+# Step 4:
+  # If swap counter greater than 0
+# Step 4a:
+  # Repeat sort
+
+# Step 4:
+  # Return sorted array
